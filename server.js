d47095bcfbca028208d92a0368b03c0c65e348d9
meng0707 <652021045@tsu.ac.th>
2024-07-03 09:52:47 +0700

    first commit

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 
 const port = process.env.PORT || 3200;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/api/users',(req, res) => {
     res.json(users);  
 })
@@ -16,9 +19,34 @@ app.get('/api/users/:id', (req, res) => {
  
  })
 
+
 app.get("/", (req, res) => { res.send("Hello! สุดหล่อ");}
 ); 
 
+
+app.put('/api/users/:id', (req, res) => {
+    const updateIndex = users.findIndex(user => user.id ===
+ Number(req.params.id)) 
+ 
+     res.send(`Update user id: '${users[updateIndex].id}' completed.`)
+ 
+ })
+
+ app.delete('/users/:id', (req, res) => {
+
+    const deletedIndex = users.findIndex(user => user.id ===
+ Number(req.params.id)) 
+ 
+    res.send(`Delete user '${users[deletedIndex].username}' completed.`)
+ 
+ })
+
+app.post('/api/users',(req,res) => {
+     users.push(req.body);
+     let json = req.body;
+     console.log(json);
+      res.send("Username: " +json.username + "inserted.")
+});
 
 app.listen(port, () => {
     console.log("Starting node.js at http://127.0.0.1:" + port +"/api/users");
